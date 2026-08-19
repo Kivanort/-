9f9492f838f9ad5acc63760321a6067cf59dad76
Kivanort <152086911+Kivanort@users.noreply.github.com>
2026-08-19 19:49:37 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  output: 'export',  // Включаем статический экспорт[reference:2][reference:3]
+  images: {
+    unoptimized: true,  // Отключаем оптимизацию изображений для статики[reference:4]
+  },
+  // Если сайт будет лежать в подпапке репозитория (https://kivanort.github.io/NavIIgator/)
+  // раскомментируй следующую строку и укажи название репозитория:
+  // basePath: '/NavIIgator',
+};
+
 export default nextConfig;
